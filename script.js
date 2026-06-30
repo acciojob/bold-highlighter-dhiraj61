6a411d534387a1d253942858d87e0ca710247535
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-06-30 08:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let strongList = document.querySelectorAll('#bold');
+
 function highlight() {
     //Write your code here
-
-
+	strongList.forEach(element => {
+		element.style.color = `rgb(0, 128, 0)`
+	});
 }
 
 
 function return_normal() {
     //Write your code here
-
+	strongList.forEach(element => {
+		element.style.color = `rgb(0, 0, 0)`;
+	})
     
 }
